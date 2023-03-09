e32056f7c465f7b778a0df322b6ba52eaac6d9e2
AleG <aleguizzo@icloud.com>
2023-03-09 14:54:52 +0100

    tailwind css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,39 @@
-<style>
-    nav {
-        padding: 1rem;
-        background-color: darkgray;
-        display: flex;
-    }
-    nav a {
-        color: #fff;
-        text-decoration: none;
-        padding-left: 10px;
-        margin-top: auto;
-        margin-bottom: auto;
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-    nav img{
-        height: 30px;
-    }
-</style>
-<!-- create simple nav bar-->
+<script>
+    import "../app.css";
+</script>
+<!-- 
+
 <nav>
     <img src="https://www.alegsoftware.ga/immagini/logo_small.png" alt="..."/>
     <a href="/">Home</a>
     <a href="/about">About</a>
     <a href="/todo">To-do Test</a>
-</nav>
+</nav> -->
+
+<nav class="p-3 border-gray-200 rounded bg-gray-50">
+    <div class="container flex flex-wrap items-center justify-between mx-auto">
+      <a href="/" class="flex items-center">
+          <img src="https://www.alegsoftware.ga/immagini/logo_small.png" class="h-6 mr-3 sm:h-10" alt="Flowbite Logo" />
+      </a>
+      
+      <div class=" w-full md:block md:w-auto" id="navbar-solid-bg">
+        <ul class="flex flex-col mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent">
+
+          <li>
+            <a href="/" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">Home</a>
+          </li>
+          <li>
+            <a href="/todo" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">To-do</a>
+          </li>
+          <li>
+            <a href="/about" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 ">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
 
 
 <slot>
--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -41,9 +41,8 @@
         {#each todos as todo}
             <todo id={todo}>
                 <div>
-                    
                     <button class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoRem(todo)}>X</button>
-                    <input type="checkbox" name="" id="" bind:checked={todo.checked}>
+                    <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2" name="" id="" bind:checked={todo.checked}>
                     <li class="inline-block">{todo.text}</li>
                 </div>
                 
