203e67432b7c834b2bc4c17c7b45c3a47c1eee27
AleG <aleguizzo@icloud.com>
2023-03-09 13:44:00 +0100

    to-do

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<style>
+    nav {
+        padding: 1rem;
+        background-color: darkgray;
+        display: flex;
+    }
+    nav a {
+        color: #fff;
+        text-decoration: none;
+        padding-left: 10px;
+        margin-top: auto;
+        margin-bottom: auto;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+    nav img{
+        height: 30px;
+    }
+</style>
+<!-- create simple nav bar-->
+<nav>
+    <img src="https://www.alegsoftware.ga/immagini/logo_small.png" alt="..."/>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/todo">To-do Test</a>
+</nav>
+
+
+<slot>
+
+</slot>
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import { bind } from "svelte/internal";
+    import "../../app.css";
+
+
+    let todos = []
+    let bindtodo
+    function todoAdd(){
+        todos.push(bindtodo)
+        todos = todos
+        bindtodo = ""
+    }
+    function todoRem(id){
+        let index = todos.indexOf(id);
+        if (index !== -1) {
+            todos.splice(index, 1);
+            todos = todos;
+        }
+    }
+    function todoEmpty(){
+        todos = []
+        todos = todos
+    }
+
+</script>
+<div class="p-5">
+    <ul>
+        <!-- Todos here-->
+        {#each todos as todo}
+            <todo id={todo}>
+                <button class="inline-block remove-button" on:click={todoRem(todo)}>X</button>
+                <li class="inline-block">{todo}</li>
+            </todo>
+        {/each}
+    </ul>
+    <form id="inputform" onsubmit="preventDefault();" class="pt-5">
+        <input id="inputtodo" type="text" placeholder="Item text..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" bind:value={bindtodo}>
+        <button id="submitbutton" class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoAdd}>Add</button>
+        <button id="emptybutton" class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoEmpty}>Empty</button>
+    
+    </form>
+</div>
