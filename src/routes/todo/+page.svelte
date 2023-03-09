2330636813ff5018a6aefda69c083e1ff1f46ad5
AleG <aleguizzo@icloud.com>
2023-03-09 13:54:26 +0100

    aaa

--- src/routes/todo/+page.svelte
+++ src/routes/todo/+page.svelte
@@ -4,11 +4,16 @@
 
 
     let todos = []
-    let bindtodo
+    let bindtodo, bindtodo_check
     function todoAdd(){
-        todos.push(bindtodo)
+        todos.push({
+            checked: false,
+            text: bindtodo,
+        })
         todos = todos
         bindtodo = ""
+
+
     }
     function todoRem(id){
         let index = todos.indexOf(id);
@@ -24,19 +29,26 @@
 
 </script>
 <div class="p-5">
-    <ul>
-        <!-- Todos here-->
-        {#each todos as todo}
-            <todo id={todo}>
-                <button class="inline-block remove-button" on:click={todoRem(todo)}>X</button>
-                <li class="inline-block">{todo}</li>
-            </todo>
-        {/each}
-    </ul>
     <form id="inputform" onsubmit="preventDefault();" class="pt-5">
         <input id="inputtodo" type="text" placeholder="Item text..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5" bind:value={bindtodo}>
         <button id="submitbutton" class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoAdd}>Add</button>
         <button id="emptybutton" class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoEmpty}>Empty</button>
     
     </form>
+    
+    <ul>
+        <!-- Todos here-->
+        {#each todos as todo}
+            <todo id={todo}>
+                <div>
+                    
+                    <button class="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2" on:click={todoRem(todo)}>X</button>
+                    <input type="checkbox" name="" id="" bind:checked={todo.checked}>
+                    <li class="inline-block">{todo.text}</li>
+                </div>
+                
+            </todo>
+        {/each}
+    </ul>
+    
 </div>
